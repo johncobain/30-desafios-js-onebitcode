6bb6b85a11664b25b583be6a67603c5ef1525d99
john cobain <andreygdsn@gmail.com>
2023-12-03 17:18:44 -0300

    ajudando na academia resolvido

--- ajudando-na-academia/script.js
+++ ajudando-na-academia/script.js
@@ -1,12 +1,34 @@
-function media(...notas){
-    let notaTotal = 0
-    for(i=0; i<notas.length; i++){
-        notaTotal += notas[i];
-    }
-    return notaTotal/notas.length;
+// function average(...numbers){
+//     let sum = 0
+//     for(i=0; i<numbers.length; i++){
+//         sum += numbers[i];
+//     }
+//     return sum/numbers.length;
+// }
+
+// // resolução rápida
+// function average(...numbers){
+//     let sum = 0;
+
+//     numbers.forEach(number =>{
+//         sum += number;
+//     });
+
+//     return avg = sum/numbers.length
+// }
+
+// //refatorando
+// function average(...numbers){
+//     const sum = numbers.reduce((accum, num) => accum + num, 0);
+//     return avg = sum/numbers.length;
+// }
+
+//reduzindo
+function average(...numbers){
+    return numbers.reduce((accum, num) => accum + num, 0)/numbers.length;
 }
 
-console.log(media(10,9,6,8,9,1,5,7));
-console.log(media(2,5,7,1,-2));
-console.log(media(10,10,10,10,9));
-console.log(media(25,75));
+console.log(average(10,9,6,8,9,1,5,7));
+console.log(average(2,5,7,1,-2));
+console.log(average(10,10,10,10,9));
+console.log(average(25,75));
